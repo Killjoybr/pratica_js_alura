6d8eea1e3495a2cba4c803d0f25268ce53ab34b7
Ruan David da Silva <112730142+Killjoybr@users.noreply.github.com>
2025-01-21 22:35:06 +0000

    completando logica do game

--- app.js
+++ app.js
@@ -10,31 +10,65 @@ let inputChute = document.querySelector("#chute");
 // Variáveis p/ lógica do jogo
 let tentativas = 0;
 let numeroSorteado;
+let comparativo;
 let numerosTentados = [];
 let mensagem = `Adivinhe um número entre 1 e ${maxNum}`;
 
+
+function gerarMensagemErro(){
+    let  mensagemErro = `Você errou, o número é ${comparativo}! Tente novamente. números tentados: ${numerosTentados}`;
+    return mensagemErro;
+}
+
+function gerarMensagemAcerto(){
+    let mensagemAcerto = `Parabens! Você acertou e o número era ${(numeroSorteado)}. Você acertou em ${tentativas}  tentativas.`;
+    return mensagemAcerto;
+}
 
 function sortearNumero(){
     numeroSorteado = Math.floor(Math.random() * maxNum) + 1; // O +1 é p/ desconsiderar o 0 ;)
 }
 
+function verificarInput(input){
+    if(isNaN(input)){
+        return false;
+    }
+    return true;
+}
+
 function verificarChute(){
     botaoReset.disabled = false;
     let chute = parseInt(inputChute.value);
-    chute == numeroSorteado ? true : false;
+    let testeInput = verificarInput(chute);
 
-    if(chute){
+    if(numerosTentados.includes(chute)){
+        alert("Você já tentou esse número, tente outro!");
+        return;
+    }
+
+    if(chute == numeroSorteado && testeInput){
         tentativas++;
-        descJogo.innerHTML = mensagemAcerto;
-    } else{
+        descJogo.innerHTML = gerarMensagemAcerto();
+        return;
+    } else if(testeInput && chute >= 1 && chute <= maxNum){
+        comparativo = chute > numeroSorteado ? "menor" : "maior";
         tentativas++;
         numerosTentados.push(chute);
-        descJogo.innerHTML = `Você errou! Tente novamente. Números tentados: ${numerosTentados.join(", ")}`;
+        descJogo.innerHTML = gerarMensagemErro();
+        return;
     }
+    
+    alert(`Digite um número válido! (entre 1 e ${maxNum})`);   
 }
 
 function resetGame(){
+    sortearNumero();
+    numerosTentados = [];
+    tentativas = 0;
 
+    descJogo.innerHTML = mensagem;
+    inputChute.value = "";
+    botaoReset.disabled = true;
 }
 
 // Função que irão executar após a página carregar
